9761ccaaa7de1d1b2b45c1f627df76e5b5bef903
AndreyKuzkov <akuzkov@viju.ru>
2022-10-17 21:29:24 +0300

    Created env variable

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,6 @@
 import { Playlist } from './models/playlist';
 import { Media } from './models/media';
-
-const BASE_URL = 'https://api.stg.testvip.ru/api/v1/';
+import { environment } from '../../env';
 
 const VISITOR_ID = '63de3b4c-b664-48fc-9717-6d5be98aa9d2';
 
@@ -12,13 +11,13 @@ export class Http {
 }
 
 export const getMainPagePlaylists = (): Promise<Playlist[]> => {
-  return Http.get<Playlist[]>(`${BASE_URL}/main_page`).then(playlists =>
+  return Http.get<Playlist[]>(`${environment.baseUrl}/main_page`).then(playlists =>
     playlists.filter(playlist => !!playlist.content_moments_list)
   );
 };
 
 export const getMediaList = (): Promise<Media[]> => {
   return Http.get<Media[]>(
-    `${BASE_URL}/contents/cold_recommendations?visitor_id=${VISITOR_ID}`
+    `${environment.baseUrl}/contents/cold_recommendations?visitor_id=${VISITOR_ID}`
   );
 };
